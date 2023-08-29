a4eea495b9edaad10c203d29682152101dfeec4a
Joe Moorhouse <joe.moorhouse@gmail.com>
2023-08-29 18:25:12 +0100

    Update maps to use react-map-gl; redesign hazard menu (#84)

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import "mapbox-gl/dist/mapbox-gl.css"
-import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import "./index.css"
 import App from "./layouts/App"
 import reportWebVitals from "./reportWebVitals"
