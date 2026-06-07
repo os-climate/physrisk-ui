1f8b5ce2975103029aba447fc92cd0d8373138d3
joemoorhouse <joe.moorhouse@gmail.com>
2026-06-07 22:08:23 +0100

    Prettier
    Signed-off-by: joemoorhouse <joe.moorhouse@gmail.com>

--- src/data/HazardInventory.js
+++ src/data/HazardInventory.js
@@ -44,7 +44,8 @@ export const hazardMenuReducer = (state, action) => {
                     action.payload.selectedIndices
                 )
             var [hazardTypeId, model, scenario, year] = selection // eslint-disable-line no-redeclare
-            var mapInfo = state.inventory.getMapInfo( // eslint-disable-line no-redeclare
+            var mapInfo = state.inventory.getMapInfo(
+                // eslint-disable-line no-redeclare
                 hazardTypeId,
                 model.path,
                 scenario.id,
